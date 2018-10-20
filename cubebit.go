268a08c4a19a54f50e0091e8c5bb8014502336d1
Ben Laurie <ben@links.org>
2018-10-20 17:13:00 +0100

    Move gamma correction into cubebit.

--- cubebit.go
+++ cubebit.go
@@ -35,6 +35,29 @@ func New(config *ws281x.HardwareConfig, sx, sy, sz int) (*Cubebit, func() error,
 	return &Cubebit{canvas: c, sizeX: sx, sizeY: sy, sizeZ: sx}, c.Close, nil
 }
 
+// gamma is a gamma correction table.
+var gamma = []uint8{
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+	2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+	5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
+	10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+	17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+	25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+	37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+	51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+	69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+	90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+	115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+	144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+	177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+	215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255}
+
+func gammafy(col color.RGBA) color.RGBA {
+	return color.RGBA{gamma[col.R], gamma[col.G], gamma[col.B], col.A}
+}
+
 // Set turns the pixel at the specified coordinates to the specified colour.
 // Note that no LEDs will change colour until the application also calls the
 // Render function, below.
@@ -46,7 +69,7 @@ func (c *Cubebit) Set(x, y, z int, col color.RGBA) {
 		x = c.sizeX - x - 1
 	}
 	page := c.sizeY * z
-	c.canvas.Set(x, page+y, col)
+	c.canvas.Set(x, page+y, gammafy(col))
 }
 
 // Render sends the colour data previously associated with LEDs using the Set
